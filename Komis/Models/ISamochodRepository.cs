0a99c9ed1704b6aa343c56c376b02c8fb53d6790
Kamil Lachowicz <kamil109111@gmail.com>
2020-07-20 12:59:18 +0200

    Utworzenie modelu i metod samochodu, dodanie usługi w startup

---
+++ Komis/Models/ISamochodRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Komis.Models
+{
+    public interface ISamochodRepository
+    {
+        // metoda zwracająca listę wszystkich samochodów
+        IEnumerable<Samochod> PobierzWszystkieSamochody();
+
+        // metoda zwracjąca samochód o podanym id
+        Samochod PobierzSamochodOId(int samochodId);
+    }
+}
---
+++ Komis/Models/MockSamochodRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Komis.Models
+{
+    public class MockSamochodRepository : ISamochodRepository
+    {
+        private List<Samochod> samochody;
+
+        public MockSamochodRepository()
+        {
+            if (samochody == null)
+            {
+                ZaladujSamochody();
+            }
+        }
+
+        private void ZaladujSamochody()
+        {
+            samochody = new List<Samochod>
+            {
+
+            };
+        }
+
+        public Samochod PobierzSamochodOId(int samochodId)
+        {
+            return samochody.FirstOrDefault(s => s.Id == samochodId);
+        }
+
+        public IEnumerable<Samochod> PobierzWszystkieSamochody()
+        {
+            return samochody;
+        }
+    }
+}
