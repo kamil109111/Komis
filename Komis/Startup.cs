bfe0d12b425d3da68d60c0f2d4b0a54498d2575a
Kamil Lachowicz <kamil109111@gmail.com>
2020-07-20 12:27:10 +0200

    Konfiguracja Startup.cs

--- Komis/Startup.cs
+++ Komis/Startup.cs
@@ -15,20 +15,24 @@ namespace Komis
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            // dodanie obsługi strony ExceptionPage, komponent używany podczas pisania aplikacji
+            // zwraca błędy w aplikacji
+            app.UseDeveloperExceptionPage();
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            // komponent który zwraca status żądania np. 400, 500
+            app.UseStatusCodePages();
+
+            // podłączenie obsługi plików statycznych np. obrazy, pliki js i inne
+            app.UseStaticFiles();
+
+            // używamy MVC z domyślnym routingiem
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
