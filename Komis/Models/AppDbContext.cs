372cd73a47b257bd1aa4fb1f52a8ac63e4cb1e5b
Kamil Lachowicz <kamil109111@gmail.com>
2020-08-07 11:59:47 +0200

    Dodanie EF Core do aplikacji

---
+++ Komis/Models/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Komis.Models
+{
+    // Ta klasa to pośrednik pomiędzy kodem a bazą danych, dziedziczy po wbudowanym DbContext
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+           
+        }
+
+        public DbSet<Samochod> Samochody { get; set; }
+    }
+}
