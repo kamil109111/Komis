eb920c9f3cdb41aab42926586f3f82cde3979625
Kamil Lachowicz <kamil109111@gmail.com>
2020-08-13 11:46:48 +0200

    Sprawdzenie poprawności modelu

--- Komis/Controllers/OpiniaController.cs
+++ Komis/Controllers/OpiniaController.cs
@@ -15,7 +15,35 @@ namespace Komis.Controllers
         {
             _opiniaRepository = opiniaRepository;
         }
+
+        [HttpGet]
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        // ta metoda działania ma byc wywoływana gdy wysyłamy żądanie post do kontrolera
+        // z form w widoku opini index
+        [HttpPost]
+        // 1 dostajemy opinie z formularza
+        public IActionResult Index(Opinia opinia)
+        {
+            //jeśli walidacja ModelState jest poprawna
+            if (ModelState.IsValid)
+            {
+                // 2 dodajemy opinie do bazy
+                _opiniaRepository.DodajOpinie(opinia);
+
+                //przekierowujemy uzytkownika do innej akcji żeby mu podziękować za dodanie opini
+                return RedirectToAction("OpiniaWyslana");
+            }
+            // jeśli Walidacja ModelState jest nie poprawna
+            // użytkownik zobaczy ten sam widok z error messages
+            return View(opinia);
+
+        }
+
+        public IActionResult OpiniaWyslana()
         {
             return View();
         }
--- Komis/Models/Opinia.cs
+++ Komis/Models/Opinia.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +9,21 @@ namespace Komis.Models
 {
     public class Opinia
     {
+        [BindNever]
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Nazwa użytkownika jest wymagana")]
         public string NazwaUzytkownika { get; set; }
+
+        [Required(ErrorMessage = "Adres email jest wymagany")]
+        [StringLength(100, ErrorMessage = "Adres email jest za długi")]
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+
+        [Required(ErrorMessage ="Wiadomość jest wymagana")]
+        [StringLength(5000, ErrorMessage ="Wiadomość jest za długa")]
         public string Wiadomosc { get; set; }
+
         public bool OczekujeOdpowiedzi { get; set; }
     }
 }
