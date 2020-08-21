ab67b7d1ee2d0fedc399c7ecb4232380607fd515
Kamil Lachowicz <kamil109111@gmail.com>
2020-08-21 12:29:04 +0200

    Przesyłanie zdjęć

--- Komis/Controllers/SamochodController.cs
+++ Komis/Controllers/SamochodController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Komis.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Komis.Controllers
@@ -10,12 +13,16 @@ namespace Komis.Controllers
     public class SamochodController : Controller
     {
         private readonly ISamochodRepository _samochodRepository;
+
+        // dostęp do zmiennych środowiskowych wwwroot
+        private IHostingEnvironment _env;
         
         // W miejsce ISamochodRepository będzie wstrzykiwania instancja SamochodRepository
         // bo tak jest zadeklarowane w Startup
-        public SamochodController(ISamochodRepository samochodRepository)
+        public SamochodController(ISamochodRepository samochodRepository, IHostingEnvironment env)
         {
             _samochodRepository = samochodRepository;
+            _env = env;
         }
         public IActionResult Index()
         {
@@ -42,8 +49,11 @@ namespace Komis.Controllers
         }
 
         // Ta metoda zwraca widok do dodania nowego samochodu
-        public IActionResult Create()
+        public IActionResult Create(string FileName)
         {
+            if (!string.IsNullOrEmpty(FileName))
+                ViewBag.ImgPath = "/Images/" + FileName;
+
             return View();
         }
 
@@ -63,12 +73,17 @@ namespace Komis.Controllers
             return View(samochod);
         }
 
-        public IActionResult Edit(int Id)
+        public IActionResult Edit(int Id, string FileName)
         {
             var samochod = _samochodRepository.PobierzSamochodOId(Id);
 
             if (samochod == null)
                 return NotFound();
+
+            if (!string.IsNullOrEmpty(FileName))
+                ViewBag.ImgPath = "/Images/" + FileName;
+            else
+                ViewBag.ImgPath = samochod.ZdjecieUrl;
 
             return View(samochod);
         }
@@ -104,6 +119,33 @@ namespace Komis.Controllers
             _samochodRepository.UsunSamochod(samochod);
 
             return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost("UploadFile")]
+        public async Task<IActionResult> UploadFile(IFormCollection form)
+        {
+            var webRoot = _env.WebRootPath;
+            var filePath = Path.Combine(webRoot.ToString() + "\\images\\" + form.Files[0].FileName);
+
+            //sprawdzamy czy plik istnieje
+            if (form.Files[0].FileName.Length>0)
+            {
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await form.Files[0].CopyToAsync(stream);
+                }
+            }
+
+            //sprawdzamy czy przychodzimy z widoku Create czy Edit
+            // jeśli string będzie pusty to wiemy że przychodzimy z create
+            // bo samochod nie ma jeszce nadanego id
+            if (Convert.ToString(form["Id"])== string.Empty || Convert.ToString(form["Id"]) == "0")
+                // przekierowanie do akcji Create
+                    return RedirectToAction(nameof(Create), new { FileName = Convert.ToString(form.Files[0].FileName) });
+
+            // w przeciwny razie przekierowujemy do akcji Edit
+            return RedirectToAction(nameof(Edit), new { FileName = Convert.ToString(form.Files[0].FileName), id = Convert.ToString(form["Id"]) });
+
         }
         
     }
