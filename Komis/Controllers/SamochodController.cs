a51bb59080d4018f611e838c43ef11558a91c2e8
Kamil Lachowicz <kamil109111@gmail.com>
2020-08-21 12:38:22 +0200

    Usuwanie zdjęcia z serwera

--- Komis/Controllers/SamochodController.cs
+++ Komis/Controllers/SamochodController.cs
@@ -113,10 +113,18 @@ namespace Komis.Controllers
 
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public IActionResult DeleteConfirmed(int id)
+        public IActionResult DeleteConfirmed(int id, string ZdjecieUrl)
         {
             var samochod = _samochodRepository.PobierzSamochodOId(id);
             _samochodRepository.UsunSamochod(samochod);
+
+            // usuwanie pliku zdjęcia z serwera
+            if (ZdjecieUrl != null)
+            {
+                var webRoot = _env.WebRootPath;
+                var filePath = Path.Combine(webRoot.ToString() + ZdjecieUrl);
+                System.IO.File.Delete(filePath);
+            }
 
             return RedirectToAction(nameof(Index));
         }
